2117695201d46ec5d2b26bec293384a55a4c7467
o.nizhnikov <o.nizhnikov@tinkoff.ru>
2017-02-15 15:22:59 +0300

    Macro complete

--- macros/src/main/scala/ru/tinkoff/tschema/macros/NamedImpl.scala
+++ macros/src/main/scala/ru/tinkoff/tschema/macros/NamedImpl.scala
@@ -5,7 +5,7 @@ import scala.reflect.macros.whitebox
 
 trait NamedImpl[T, Input] {
   type Output
-  def produce(input: Input): Output = ???
+  def produce(input: Input, impl: T): Output
 }
 
 object NamedImpl {
@@ -19,34 +19,46 @@ object NamedImpl {
 class NamedImplMacros(val c: whitebox.Context) extends shapeless.CaseClassMacros {
   import c.universe._
   import NamedImplMacros._
+  import internal.{constantType, refinedType}
   type =?>[-A, +B] = PartialFunction[A, B]
 
-  def materialize[T: WeakTypeTag, Input: WeakTypeTag]: Tree = {
+  def taggedType = typeOf[shapeless.tag.Tagged[_]].typeConstructor
 
-    val applier = new Applier(weakTypeOf[T], weakTypeOf[Input])
+  def materialize[T: WeakTypeTag, Input: WeakTypeTag]: Tree = try {
+    val applier = new Applier(impl = weakTypeOf[T], input = weakTypeOf[Input])
     import applier._
-    info(impl)
-
-    info(inputWiden)
-    info(s"methods: $methods")
-    info(s"union  $union")
-    if (!verifyTypes) abort("could not satisfy input type with implementation type")
+//    info(impl)
+//    info(inputWiden)
+//    info(s"methods: $methods")
+//    info(s"union  $union")
+//    info(s"output  $output")
+//    info(s"matches $matches")
+    errors.foreach(err ⇒ info("ERROR during Named Implementation : " + err))
+    if (!correct) abort("could not satisfy input type with implementation type")
 
     q"""new _root_.ru.tinkoff.tschema.macros.NamedImpl[$impl, $input]{
         import shapeless._
         type Output = $output
         override def toString = "Hello from Generic materialize\n"
-     }"""
+        def produce(union: $input, impl: $impl): Output = {
+           val input = union.values
+           $implementation
+        }
+      }"""
+  } catch {
+    case ex: Throwable ⇒
+      info("ERROR during Named Implementation : " + ex)
+      EmptyTree
   }
 
   def info[A](u: A) = c.info(c.enclosingPosition, u.toString, force = true)
   def warning(msg: String) = c.warning(c.enclosingPosition, msg)
 
-  def extractMethods(tpe: Type): NList[List[NList[Type]]] =
+  def extractMethods(tpe: Type): NList[(List[NList[Type]], Type)] =
     if (!tpe.typeSymbol.isClass || !tpe.typeSymbol.isAbstract) List()
     else tpe.decls.collect {
-      case s: MethodSymbol ⇒ s.name.decodedName.toString ->
-                             s.paramLists.map(lst ⇒ lst.map(p ⇒ p.name.decodedName.toString → p.typeSignature))
+      case s: MethodSymbol ⇒ symbolName(s) ->
+                             (s.paramLists.map(lst ⇒ lst.map(p ⇒ symbolName(p) → p.typeSignature)) -> s.returnType)
     }.toList
 
   def extractUnion(tpe: Type): NList[NList[Type]] =
@@ -55,44 +67,75 @@ class NamedImplMacros(val c: whitebox.Context) extends shapeless.CaseClassMacros
   def extractList(tpe: Type): NList[Type] =
     hlistElements(tpe).collect { case FieldType(KeyName(name), value) ⇒ name -> value }
 
+  def symbolName(symbol: Symbol) = symbol.name.decodedName.toString
 
+  case class TraverseState(correct: Boolean = true,
+                           output: Vector[Type] = Vector.empty,
+                           errors: Vector[String] = Vector.empty,
+                           matches: Vector[(Tree, Tree)] = Vector.empty) {
+    def failed(msg: String) = copy(correct = false, errors = errors :+ msg)
+    def addType(tpe: Type) = copy(output = output :+ tpe)
+    def addMatch(mat: (Tree, Tree)) = copy(matches = matches :+ mat)
+  }
 
-  class Applier(val input: Type, val impl: Type){
+  class Applier(val input: Type, val impl: Type) {
     val inputWiden = input.dealias.widen
     val union = extractUnion(inputWiden)
     val methods = extractMethods(impl)
 
-    def verifyTypes: Boolean = {
-      val implMap = methods.iterator.map { case (n, lists) ⇒ n -> lists.iterator.flatMap(_.iterator) }.toMap
-      union.foldLeft(true) { case (result, (caseName, inputFields)) ⇒
+    lazy val TraverseState(correct, outputTypes, errors, matches) = {
+      val implMap = methods.toMap
+      union.foldLeft(TraverseState()) { case (state, (caseName, inputFields)) ⇒
         implMap.get(caseName) match {
-          case None ⇒
-            info(s"ERROR: implementation for $caseName is not defined")
-            false
-          case Some(caseImpl) ⇒
-            val inputMap = inputFields.toMap
-            caseImpl.foldLeft(result) { case (result2, (paramName, parImplType)) ⇒
-              inputMap.get(paramName) match {
-                case None ⇒
-                  info(s"ERROR parameter $paramName in method $caseName not found in input")
-                  false
-                case Some(parInputType) if parInputType <:< parImplType ⇒
-                  result2
-                case Some(parInputType) ⇒
-                  info(s"ERROR parameter $paramName in method $caseName has type $parImplType not assignable from $parInputType in input")
-                  false
-              }
-            }
+          case None ⇒ state.failed(s"implementation for $caseName is not defined")
+          case Some((caseImpl, retType)) ⇒
+            traverseSingle(state, inputFields, caseImpl, retType, caseName)
         }
       }
     }
 
-    def output: Type = input
+    def traverseSingle(init: TraverseState, input: NList[Type], impl: List[NList[Type]], retType: Type, name: String) = {
+      val inputMap = input.toMap
+      val retFld = FieldType(KeyName(name), retType)
 
+      val res = impl.flatten.foldLeft(init addType retType) { case (state, (paramName, parImplType)) ⇒
+        inputMap.get(paramName) match {
+          case None ⇒ state.failed(s"parameter $paramName in method $name not found in input")
+          case Some(parInputType) if parInputType <:< parImplType ⇒ state
+          case Some(parInputType) ⇒
+            state.failed(s"parameter $paramName in method $name has type $parImplType not assignable from $parInputType in input")
+        }
+      }
 
+      def matc: (Tree, Tree) = {
+        val nameMap = input.map { case (paramName, _) ⇒ paramName -> c.freshName(paramName) }.toMap
+        val pattern = input
+                      .map { case (pname, _) ⇒ TermName(nameMap(pname)) }
+                      .foldRight[Tree](pq"HNil")((term, tail) ⇒ pq"$term :: $tail")
+
+        val paramLists = impl.map(_ map { case (paramName, _) ⇒ Ident(TermName(nameMap(paramName))) })
+        val funcName = TermName(name)
+        val result = q"impl.$funcName(...$paramLists)"
+        pattern → result
+      }
+
+      if (res.correct) res addMatch matc else res
+    }
+
+    lazy val output = mkCoproductTpe(outputTypes.toList)
+
+    lazy val implementation = matches.foldRight[Tree](q"input") {
+      case ((pat, res), next) ⇒ q"""input match{
+          case Inl($pat) => Inl($res)
+          case Inr(input) => Inr($next)
+        } """
+    }
   }
 
   object KeyName {
+    def apply(name: String): Type =
+      NamedSymbol(appliedType(taggedType, constantType(Constant(name))))
+
     def unapply(tpe: Type): Option[String] = tpe match {
       case NamedSymbol(ConstantType(Constant(name: String))) ⇒ Some(name)
       case _ ⇒ None
@@ -100,14 +143,13 @@ class NamedImplMacros(val c: whitebox.Context) extends shapeless.CaseClassMacros
   }
 
   object NamedSymbol {
+    def apply(tpe: Type) = refinedType(List(symbolTpe, tpe), NoSymbol)
+
     def unapply(tpe: Type) = tpe match {
-      case RefinedType(List(sym, tag, _*), _)
-        if sym == symbolTpe ⇒ tag.typeArgs.headOption
+      case RefinedType(List(sym, tag, _*), _) if sym == symbolTpe ⇒ tag.typeArgs.headOption
       case _ ⇒ None
     }
   }
-
-  c.freshName()
 }
 
 object NamedImplMacros {
--- macros/src/test/scala-2.12/Checks.scala
+++ macros/src/test/scala-2.12/Checks.scala
@@ -6,29 +6,32 @@ import shapeless.syntax.singleton._
 import shapeless.union._
 import shapeless.{Witness ⇒ W}
 import shapeless.labelled._
+import shapeless.tag.{@@, Tagged}
 
 object toProduct extends Poly1 {
   implicit def labelled[T](implicit lgen: LabelledGeneric[T]): Case.Aux[T, lgen.Repr] = at[T](t ⇒ lgen.to(t))
 }
 
-object Checks {
-
+object Checks extends App {
 
   import scala.concurrent.Future
   sealed trait DatabaseInput
-  final case class put(id: Int, value: String) extends DatabaseInput
+  final case class put(id: Long, value: String) extends DatabaseInput
   final case class read(id: Long, session: Long) extends DatabaseInput
+//  final case class delete(id: Long) extends DatabaseInput
 
-  val inputCopro = LabelledGeneric[DatabaseInput].to(put(1, "adasd"))
+  val inputCopro = LabelledGeneric[DatabaseInput].to(read(2, 123))
   val inputGen = inputCopro.mapValues(toProduct)
 
   LabelledGeneric[put]
 
   trait DatabaseService {
-    def put(value: String, id: Int): Future[Unit]
-    def read(id: Any): Future[Long]
-    def zzz(u: Double): Unit = ???
+    def put(value: String)(id: Long) = s"$value $id"
+    def read(id: AnyVal) = println(id)
+    def zzz(u: Double): Unit = ()
   }
 
-  NamedImpl[DatabaseService, inputGen.type]
+  val result = NamedImpl[DatabaseService, inputGen.type]
+  print(result.produce(inputGen, new DatabaseService {}))
 }
+

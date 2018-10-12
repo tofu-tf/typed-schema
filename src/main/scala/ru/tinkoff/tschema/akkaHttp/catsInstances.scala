2934968c52998fe4bc37872add2d1d375ffef2ba
Daniel Slapman <danslapman@gmail.com>
2018-10-12 17:38:01 +0300

    Bump cats to 1.4.0, use FunctorFilter, remove cats-mtl

--- src/main/scala/ru/tinkoff/tschema/akkaHttp/catsInstances.scala
+++ src/main/scala/ru/tinkoff/tschema/akkaHttp/catsInstances.scala
@@ -1,13 +1,12 @@
 package ru.tinkoff.tschema.akkaHttp
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{Directive, Directive1, Rejection}
-import cats.mtl.FunctorEmpty
-import cats.{Monad, MonoidK}
+import cats.{Monad, MonoidK, FunctorFilter}
 
 object catsInstances {
   case object FilterRejection extends Rejection
 
-  implicit val directive1Instance = new Monad[Directive1] with MonoidK[Directive1] with FunctorEmpty[Directive1]{
+  implicit val directive1Instance = new Monad[Directive1] with MonoidK[Directive1] with FunctorFilter[Directive1]{
     def pure[A](x: A): Directive1[A] = provide(x)
     def empty[A]: Directive1[A] = Directive(_ => reject(FilterRejection))
     def flatMap[A, B](fa: Directive1[A])(f: (A) => Directive1[B]): Directive1[B] = fa.flatMap(f)
@@ -19,8 +18,6 @@ object catsInstances {
     override def filter[A](fa: Directive1[A])(f: (A) => Boolean): Directive1[A] = fa.filter(f)
     override def map[A, B](fa: Directive1[A])(f: (A) => B): Directive1[B] = fa.map(f)
     val functor = this
-    def mapFilter[A, B](fa: Directive1[A])(f: (A) => Option[B]) = ???
-    def collect[A, B](fa: Directive1[A])(f: PartialFunction[A, B]) = ???
-    def flattenOption[A](fa: Directive1[Option[A]]) = ???
+    override def mapFilter[A, B](fa: Directive1[A])(f: A => Option[B]): Directive1[B] = ???
   }
 }

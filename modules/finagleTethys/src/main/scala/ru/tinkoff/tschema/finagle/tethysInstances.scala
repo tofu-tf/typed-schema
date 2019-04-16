1c94d008477c77c99a702ffc00f6d2b3be6b7d57
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2019-04-16 18:00:04 +0300

    json instances

--- modules/finagleTethys/src/main/scala/ru/tinkoff/tschema/finagle/tethysInstances.scala
+++ modules/finagleTethys/src/main/scala/ru/tinkoff/tschema/finagle/tethysInstances.scala
@@ -1,10 +1,6 @@
 package ru.tinkoff.tschema.finagle
-import Routed.implicits._
 import cats.Applicative
-import cats.syntax.applicative._
-import cats.syntax.flatMap._
-import com.twitter.finagle.http.{Response, Status, Version}
-import com.twitter.io.{Buf, Reader}
+import ru.tinkoff.tschema.finagle.util.message.{jsonBodyParse, jsonComplete}
 import tethys._
 import tethys.readers.tokens.TokenIteratorProducer
 import tethys.writers.tokens.TokenWriterProducer
@@ -12,12 +8,10 @@ import tethys.writers.tokens.TokenWriterProducer
 object tethysInstances {
   implicit def tethysEncodeComplete[F[_]: Applicative, A: JsonWriter](
       implicit producer: TokenWriterProducer = jackson.jacksonTokenWriterProducer): Complete[F, A] =
-    a => Response(Version.Http10, Status.Ok, Reader.fromBuf(Buf.Utf8(a.asJson))).pure[F]
+    jsonComplete(_.asJson)
 
   implicit def tethysDecodeParseBody[F[_]: Routed, A: JsonReader](
       implicit producer: TokenIteratorProducer = jackson.jacksonTokenIteratorProducer
   ): ParseBody[F, A] =
-    () =>
-      Routed.request.flatMap(req =>
-        req.contentString.jsonAs[A].fold(fail => Routed.reject(Rejection.body(fail.getMessage)), res => res.pure[F]))
+    jsonBodyParse(_.jsonAs[A])
 }

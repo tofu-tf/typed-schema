620d232e13967b4464042cfc099ac5896d9bc5d2
Odomontois <odomontois@gmail.com>
2019-07-24 20:21:06 +0300

    merge examples

---
+++ examples/finagle/src/main/scala/ru/tinkoff/tschema/example/ExampleEnv.scala
@@ -0,0 +1,15 @@
+package ru.tinkoff.tschema.example
+
+import ru.tinkoff.tschema.finagle.{Routed, Runnable}
+import ru.tinkoff.tschema.finagle.zioInstance._
+import zio.console._
+import zio.{Ref, ZIO}
+
+final case class ExampleEnv(trackingId: String, alohas: Ref[Int]) extends Console.Live
+
+object ExampleEnv {
+  final implicit val httpRouted: Routed[Http]         = zioRouted
+  final implicit val httpRun: Runnable[Http, Example] = zioRunnable()
+
+  val incrementAlohas: Http[Int] = ZIO.accessM(_.embedded.alohas.update(_ + 1))
+}
---
+++ examples/finagle/src/main/scala/ru/tinkoff/tschema/example/package.scala
@@ -0,0 +1,10 @@
+package ru.tinkoff.tschema
+
+import ru.tinkoff.tschema.finagle._
+import ru.tinkoff.tschema.finagle.zioInstance._
+import zio._
+
+package object example {
+  type Http[+A]    = ZIOHttp[ExampleEnv, Nothing, A]
+  type Example[+A] = ZIO[ExampleEnv, Nothing, A]
+}

722304421d2712dc44f2b70223fc3ead1c121b8f
¨susliko¨ <1istoobig@gmail.com>
2019-07-29 10:41:32 +0300

    Dependencies updated: circe: 0.11.1 -> 0.12.0-M3 derevo: 0.8.0 -> 0.8.1

--- modules/swagger/src/main/scala/ru/tinkoff/tschema/utils/json/circeSyntax.scala
+++ modules/swagger/src/main/scala/ru/tinkoff/tschema/utils/json/circeSyntax.scala
@@ -3,8 +3,8 @@ package ru.tinkoff.tschema.utils.json
 import io.circe._
 
 object circeSyntax {
-  implicit class ObjectEncoderOps[A](val enc: ObjectEncoder[A]) extends AnyVal {
-    def mapObjWithSrc(f: (A, JsonObject) => JsonObject): ObjectEncoder[A] = ObjectEncoder.instance[A] {
+  implicit class ObjectEncoderOps[A](val enc: Encoder.AsObject[A]) extends AnyVal {
+    def mapObjWithSrc(f: (A, JsonObject) => JsonObject): Encoder.AsObject[A] = Encoder.AsObject.instance[A] {
       x => f(x, enc.encodeObject(x))
     }
   }

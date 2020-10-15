96d4e77ca1a310671b565351ccd2ee86c1205413
susliko <1istoobig@gmail.com>
2020-10-15 13:05:05 +0300

    fix SwaggerMapperSpec for 2.12

--- modules/swagger/src/test/scala/ru/tinkoff/tschema/swagger/SwaggerMapperSpec.scala
+++ modules/swagger/src/test/scala/ru/tinkoff/tschema/swagger/SwaggerMapperSpec.scala
@@ -3,18 +3,20 @@ package ru.tinkoff.tschema.swagger
 import derevo.derive
 import org.scalatest.flatspec.AnyFlatSpec
 import ru.tinkoff.tschema.typeDSL._
+import shapeless.Witness
 
 @derive(Swagger)
 case class Enterprise(market: String, product: Int)
 
 class SwaggerMapperSpec extends AnyFlatSpec {
   "type schemas" should "be the same for all types of params" in {
-    val query     = implicitly[SwaggerMapper[QueryParam["", Enterprise]]].types
-    val formField = implicitly[SwaggerMapper[FormField["", Enterprise]]].types
-    val header    = implicitly[SwaggerMapper[Header["", Enterprise]]].types
-    val cookie    = implicitly[SwaggerMapper[Cookie["", Enterprise]]].types
-    val capture   = implicitly[SwaggerMapper[Capture["", Enterprise]]].types
-    val reqBody   = implicitly[SwaggerMapper[ReqBody["", Enterprise]]].types
+    type name = Witness.`""`.T
+    val query     = implicitly[SwaggerMapper[QueryParam[name, Enterprise]]].types
+    val formField = implicitly[SwaggerMapper[FormField[name, Enterprise]]].types
+    val header    = implicitly[SwaggerMapper[Header[name, Enterprise]]].types
+    val cookie    = implicitly[SwaggerMapper[Cookie[name, Enterprise]]].types
+    val capture   = implicitly[SwaggerMapper[Capture[name, Enterprise]]].types
+    val reqBody   = implicitly[SwaggerMapper[ReqBody[name, Enterprise]]].types
     assert(query == formField)
     assert(formField == header)
     assert(header == cookie)
